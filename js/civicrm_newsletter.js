43fbf30f6d2061b53fa22be302f737b37e8e01e5
Jens Schuppe <schuppe@systopia.de>
2018-08-02 14:44:38 +0200

    Build a fieldsets/checkboxes tree from the group tree received with the profile.

--- js/civicrm_newsletter.js
+++ js/civicrm_newsletter.js
@@ -1,24 +1,24 @@
 (function ($) {
   Drupal.behaviors.civicrm_newsletter = {
     attach: function (context, settings) {
-      var $mailingLists = $('.form-type-checkboxes.form-item-mailing-lists', context);
+      var $mailingLists = $('.form-item-mailing-lists', context);
       var $checkboxes = $mailingLists.find('.form-type-checkbox').find('input[type="checkbox"]');
       var updateToggleLabel = function($mailingLists) {
         var $checkboxes = $mailingLists.find('.form-type-checkbox').find('input[type="checkbox"]');
         $mailingLists.find('.toggle-all').text(($checkboxes.length === $checkboxes.filter('[checked]').length ? Drupal.t('Deselect all') : Drupal.t('Select all')));
       };
-      $mailingLists.children('.form-checkboxes').prepend(
+      $mailingLists.children('.fieldset-wrapper').prepend(
         $('<a>')
           .attr('href', '#')
           .addClass('toggle-all')
           .text(($checkboxes.length === $checkboxes.filter('[checked]').length ? Drupal.t('Deselect all') : Drupal.t('Select all')))
           .click(function(event) {
-            var $checkboxes = $(this).siblings().find('input[type="checkbox"]');
+            var $checkboxes = $(this).parent().find('input[type="checkbox"]');
             if ($checkboxes.length === $checkboxes.filter('[checked]').length) {
-              $(this).siblings().find('input[type="checkbox"]').removeAttr('checked');
+              $(this).parent().find('input[type="checkbox"]').removeAttr('checked');
             }
             else {
-              $(this).siblings().find('input[type="checkbox"]').attr('checked', 'checked');
+              $(this).parent().find('input[type="checkbox"]').attr('checked', 'checked');
             }
             updateToggleLabel($mailingLists);
             event.preventDefault();
