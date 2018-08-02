88eb98740ed56c8ab3465cb4a2ef9b852021facc
Jens Schuppe <schuppe@systopia.de>
2018-08-02 14:51:49 +0200

    Add the mailing lists "select all" toggle switch after the fieldset description.

--- js/civicrm_newsletter.js
+++ js/civicrm_newsletter.js
@@ -7,23 +7,22 @@
         var $checkboxes = $mailingLists.find('.form-type-checkbox').find('input[type="checkbox"]');
         $mailingLists.find('.toggle-all').text(($checkboxes.length === $checkboxes.filter('[checked]').length ? Drupal.t('Deselect all') : Drupal.t('Select all')));
       };
-      $mailingLists.children('.fieldset-wrapper').prepend(
-        $('<a>')
-          .attr('href', '#')
-          .addClass('toggle-all')
-          .text(($checkboxes.length === $checkboxes.filter('[checked]').length ? Drupal.t('Deselect all') : Drupal.t('Select all')))
-          .click(function(event) {
-            var $checkboxes = $(this).parent().find('input[type="checkbox"]');
-            if ($checkboxes.length === $checkboxes.filter('[checked]').length) {
-              $(this).parent().find('input[type="checkbox"]').removeAttr('checked');
-            }
-            else {
-              $(this).parent().find('input[type="checkbox"]').attr('checked', 'checked');
-            }
-            updateToggleLabel($mailingLists);
-            event.preventDefault();
-          })
-      );
+      var $toggleSwitch = $('<a>')
+        .attr('href', '#')
+        .addClass('toggle-all')
+        .text(($checkboxes.length === $checkboxes.filter('[checked]').length ? Drupal.t('Deselect all') : Drupal.t('Select all')))
+        .click(function(event) {
+          var $checkboxes = $(this).parent().find('input[type="checkbox"]');
+          if ($checkboxes.length === $checkboxes.filter('[checked]').length) {
+            $(this).parent().find('input[type="checkbox"]').removeAttr('checked');
+          }
+          else {
+            $(this).parent().find('input[type="checkbox"]').attr('checked', 'checked');
+          }
+          updateToggleLabel($mailingLists);
+          event.preventDefault();
+        });
+      $mailingLists.children('.fieldset-wrapper').prepend($toggleSwitch).prepend($toggleSwitch.next('.fieldset-description'));
       $checkboxes.click(function() {
         updateToggleLabel($mailingLists);
       });
