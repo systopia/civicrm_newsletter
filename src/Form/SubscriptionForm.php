08057e7d075e8983ade3b2c4a157a1c8df6c2a3c
B. Zschiedrich <zschiedrich@systopia.de>
2020-10-29 12:55:49 +0100

    Subscription form stub

---
+++ src/Form/SubscriptionForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\civicrm_newsletter\Form;
+
+use Drupal\Core\Form\FormInterface;
+# use Drupal\Core\Form\FormBase;
+# use Drupal\Core\Form\FormStateInterface;
+# FIXME: Which to use?
+
+class SubscriptionForm implements FormInterface # FIXME: Or "extends FormBase" instead?
+{
+    public function getFormID()
+    {
+        // TODO: Implement.
+    }
+
+    public function buildForm(array $form, array &$form_state)
+    {
+        // TODO: Implement.
+
+        return $form;
+    }
+
+    public function validateForm(array &$form, array &$form_state)
+    {
+        // TODO: Implement.
+    }
+
+    public function submitForm(array &$form, array &$form_state)
+    {
+        // TODO: Implement.
+    }
+}
