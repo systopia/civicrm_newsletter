517d82d5e9894e25d4c164fb324db9754182b5ca
Jens Schuppe <schuppe@systopia.de>
2021-10-26 12:03:26 +0200

    Add configurable redirect target for unsubscriptions

--- src/Form/PreferencesForm.php
+++ src/Form/PreferencesForm.php
@@ -95,111 +95,113 @@ class PreferencesForm extends FormBase {
         ['profile' => $profile->name]
       );
     }
-    elseif ($config->get('preferences_autoconfirm')) {
-      // Automatically confirm pending subscriptions.
-      $result = $this->cmrf->subscriptionAutoconfirm(array(
-        'contact_id' => $subscription['contact']['id'],
-        'contact_hash' => $subscription['contact']['hash'],
-        'profile' => $profile->name,
-      ));
-      if (!empty($result['is_error'])) {
-        // The API call returned an error, rebuild the form and notify the user.
-        Drupal::messenger()->addError(
-          $this->t('Your confirmation of pending subscriptions could not be submitted, please try again later.')
-        );
-        $form_state->setRebuild();
-      }
-      elseif (!empty($result['values'])) {
-        Drupal::messenger()->addStatus(
-          $this->t('Your confirmation of pending subscriptions has been successfully submitted. You will receive an e-mail with a summary of your subscriptions.')
-        );
-      }
-      else {
-        Drupal::messenger()->addStatus(
-          $this->t('Your confirmation of pending subscriptions has been successfully submitted, but no subscriptions were pending to be confirmed.')
-        );
-      }
-
-      $form['contact_hash'] = array(
-        '#type' => 'value',
-        '#value' => $subscription['contact']['hash'],
-      );
-      $form['contact_id'] = array(
-        '#type' => 'value',
-        '#value' => $subscription['contact']['id'],
-      );
-
-      // Build form according to retrieved configuration:
-      // Add contact fields.
-      foreach ($profile->contact_fields as $contact_field_name => $contact_field) {
-        if ($contact_field['active']) {
-          $form[$contact_field_name] = array(
-            '#type' => Utils::contactFieldTypes()[$contact_field['type']],
-            '#title' => $contact_field['label'],
-            '#description' => $contact_field['description'],
-            '#default_value' => $subscription['contact'][$contact_field_name],
-            '#required' => !empty($contact_field['required']),
-            '#disabled' => TRUE,
+    if ($config->get('preferences_autoconfirm')) {
+      if (empty($form_state->getUserInput())) {
+        // Automatically confirm pending subscriptions.
+        $result = $this->cmrf->subscriptionAutoconfirm([
+          'contact_id' => $subscription['contact']['id'],
+          'contact_hash' => $subscription['contact']['hash'],
+          'profile' => $profile->name,
+        ]);
+        if (!empty($result['is_error'])) {
+          // The API call returned an error, rebuild the form and notify the user.
+          Drupal::messenger()->addError(
+            $this->t('Your confirmation of pending subscriptions could not be submitted, please try again later.')
           );
-          if (!empty($contact_field['options'])) {
-            $form[$contact_field_name]['#options'] = $contact_field['options'];
-            if (empty($contact_field['required'])) {
-              $form[$contact_field_name]['#empty_option'] = t('- None -');
-            }
+          $form_state->setRebuild();
+        }
+        elseif (!empty($result['values'])) {
+          Drupal::messenger()->addStatus(
+            $this->t('Your confirmation of pending subscriptions has been successfully submitted. You will receive an e-mail with a summary of your subscriptions.')
+          );
+        }
+        else {
+          Drupal::messenger()->addStatus(
+            $this->t('Your confirmation of pending subscriptions has been successfully submitted, but no subscriptions were pending to be confirmed.')
+          );
+        }
+      }
+    }
+
+    $form['contact_hash'] = [
+      '#type' => 'value',
+      '#value' => $subscription['contact']['hash'],
+    ];
+    $form['contact_id'] = [
+      '#type' => 'value',
+      '#value' => $subscription['contact']['id'],
+    ];
+
+    // Build form according to retrieved configuration:
+    // Add contact fields.
+    foreach ($profile->contact_fields as $contact_field_name => $contact_field) {
+      if ($contact_field['active']) {
+        $form[$contact_field_name] = [
+          '#type' => Utils::contactFieldTypes()[$contact_field['type']],
+          '#title' => $contact_field['label'],
+          '#description' => $contact_field['description'],
+          '#default_value' => $subscription['contact'][$contact_field_name],
+          '#required' => !empty($contact_field['required']),
+          '#disabled' => TRUE,
+        ];
+        if (!empty($contact_field['options'])) {
+          $form[$contact_field_name]['#options'] = $contact_field['options'];
+          if (empty($contact_field['required'])) {
+            $form[$contact_field_name]['#empty_option'] = t('- None -');
           }
         }
       }
-
-      // Add mailing lists selection.
-      $form['mailing_lists'] = Utils::mailingListsTreeCheckboxes(
-        $profile->mailing_lists_tree,
-        $subscription['subscription_status']
-      );
-      $form['mailing_lists']['#title'] = $profile->mailing_lists_label;
-      $form['mailing_lists']['#description'] = $profile->mailing_lists_description;
-      $form['mailing_lists']['#attributes'] = array(
-        'class' => array(
-          'form-item-mailing-lists',
-        ),
-      );
-      $form['mailing_lists']['#attached']['library'][] = 'civicrm_newsletter/civicrm_newsletter';
-
-      if (!empty($profile->mailing_lists_unsubscribe_all)) {
-        $form['unsubscribe'] = array(
-          '#type' => 'fieldset',
-          '#title' => $profile->mailing_lists_unsubscribe_all_label,
-        );
-        $form['unsubscribe']['unsubscribe_all'] = array(
-          '#type' => 'checkbox',
-          '#title' => $profile->mailing_lists_unsubscribe_all_submit_label,
-          '#description' => $profile->mailing_lists_unsubscribe_all_description
-        );
-        $form['mailing_lists']['#states'] = array(
-          'invisible' => array(
-            ':input[name="unsubscribe_all"]' => array(
-              'checked' => TRUE,
-            ),
-          ),
-        );
-      }
-
-      // Add terms and conditions.
-      if (!empty($profile->conditions_preferences)) {
-        $form['conditions_preferences'] = array(
-          '#type' => 'textarea',
-          '#title' => $profile->conditions_preferences_label,
-          '#description' => $profile->conditions_preferences_description,
-          '#value' => $profile->conditions_preferences,
-          '#disabled' => TRUE,
-        );
-      }
-
-      // Add submit button with configured label, if given.
-      $form['submit'] = array(
-        '#type' => 'submit',
-        '#value' => $profile->submit_label ?: t('Submit'),
-      );
     }
+
+    // Add mailing lists selection.
+    $form['mailing_lists'] = Utils::mailingListsTreeCheckboxes(
+      $profile->mailing_lists_tree,
+      $subscription['subscription_status']
+    );
+    $form['mailing_lists']['#title'] = $profile->mailing_lists_label;
+    $form['mailing_lists']['#description'] = $profile->mailing_lists_description;
+    $form['mailing_lists']['#attributes'] = [
+      'class' => [
+        'form-item-mailing-lists',
+      ],
+    ];
+    $form['mailing_lists']['#attached']['library'][] = 'civicrm_newsletter/civicrm_newsletter';
+
+    if (!empty($profile->mailing_lists_unsubscribe_all)) {
+      $form['unsubscribe'] = [
+        '#type' => 'fieldset',
+        '#title' => $profile->mailing_lists_unsubscribe_all_label,
+      ];
+      $form['unsubscribe']['unsubscribe_all'] = [
+        '#type' => 'checkbox',
+        '#title' => $profile->mailing_lists_unsubscribe_all_submit_label,
+        '#description' => $profile->mailing_lists_unsubscribe_all_description
+      ];
+      $form['mailing_lists']['#states'] = [
+        'invisible' => [
+          ':input[name="unsubscribe_all"]' => [
+            'checked' => TRUE,
+          ],
+        ],
+      ];
+    }
+
+    // Add terms and conditions.
+    if (!empty($profile->conditions_preferences)) {
+      $form['conditions_preferences'] = [
+        '#type' => 'textarea',
+        '#title' => $profile->conditions_preferences_label,
+        '#description' => $profile->conditions_preferences_description,
+        '#value' => $profile->conditions_preferences,
+        '#disabled' => TRUE,
+      ];
+    }
+
+    // Add submit button with configured label, if given.
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $profile->submit_label ?: t('Submit'),
+    ];
 
     return $form;
   }
@@ -229,26 +231,44 @@ class PreferencesForm extends FormBase {
     if (!empty($result['is_error'])) {
       // The API call returned an error, rebuild the form and notify the user.
       Drupal::messenger()->addError(
-        $this->t('Your confirmation could not be submitted, please try again later.')
+        $this->t('Your subscription preferences could not be submitted, please try again later.')
       );
       $form_state->setRebuild();
     }
+    elseif ($params['unsubscribe_all']) {
+      Drupal::messenger()->addStatus(
+        $this->t('Your unsubscription has been successfully submitted. You will receive an e-mail with a confirmation of your unsubscription.')
+      );
+    }
     else {
       Drupal::messenger()->addStatus(
-        $this->t('Your confirmation has been successfully submitted. You will receive an e-mail with a summary of your subscriptions.')
+        $this->t('Your subscription preferences have been successfully submitted. You will receive an e-mail with a summary of your subscriptions.')
       );
+    }
 
-      // Redirect to target from configuration.
-      if (!empty($redirect_path = $config->get('redirect_paths.preferences_form'))) {
-        /* @var Url $url */
-        $url = Drupal::service('path.validator')
-          ->getUrlIfValid($redirect_path);
-        $form_state->setRedirect(
-          $url->getRouteName(),
-          $url->getRouteParameters(),
-          $url->getOptions()
-        );
-      }
+    // Redirect to target from configuration.
+    if (
+      $params['unsubscribe_all']
+      && !empty($redirect_path = $config->get('redirect_paths.unsubscribe'))
+    ) {
+      /* @var Url $url */
+      $url = Drupal::service('path.validator')
+        ->getUrlIfValid($redirect_path);
+      $form_state->setRedirect(
+        $url->getRouteName(),
+        $url->getRouteParameters(),
+        $url->getOptions()
+      );
+    }
+    elseif (!empty($redirect_path = $config->get('redirect_paths.preferences_form'))) {
+      /* @var Url $url */
+      $url = Drupal::service('path.validator')
+        ->getUrlIfValid($redirect_path);
+      $form_state->setRedirect(
+        $url->getRouteName(),
+        $url->getRouteParameters(),
+        $url->getOptions()
+      );
     }
   }
 
