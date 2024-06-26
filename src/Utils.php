6a0d1d57e601decd2b69e86b4747c49f4ac04f04
Sven Nowotnik <nowotnik@systopia.de>
2024-06-26 08:04:06 +0200

    added 'Number' as CiviCRM HTML types to be mapped to Drupal Form Type 'textfield' in order to prevent type missmatch warnings/errors when a number field is being used in newsletter-form configuration on civicrm-side

--- src/Utils.php
+++ src/Utils.php
@@ -29,6 +29,7 @@ class Utils {
     return array(
       'Select' => 'select',
       'Multi-Select' => 'checkboxes',
+      'Number' => 'textfield',
       'Text' => 'textfield',
       'Textarea' => 'textarea',
       'CheckBox' => 'checkbox',
