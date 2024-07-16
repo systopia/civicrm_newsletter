fba03ec01cf462aabc7117bf7e085fc745255182
Sven Nowotnik <nowotnik@systopia.de>
2024-07-16 13:42:37 +0200

    changed "contact-fields" => "contact_fields"

--- src/Form/PreferencesForm.php
+++ src/Form/PreferencesForm.php
@@ -137,7 +137,7 @@ class PreferencesForm extends FormBase {
     // Add contact fields.
     foreach ($profile->contact_fields as $contact_field_name => $contact_field) {
       if (isset($contact_field['active']) && $contact_field['active']) {
-        $form['contact-fields'][$contact_field_name] = [
+        $form['contact_fields'][$contact_field_name] = [
           '#type' => Utils::getContactFieldType($contact_field),
           '#title' => $contact_field['label'],
           '#description' => $contact_field['description'],
@@ -149,9 +149,9 @@ class PreferencesForm extends FormBase {
                     ? $contact_field['weight'] : 0,
         ];
         if (!empty($contact_field['options'])) {
-          $form['contact-fields'][$contact_field_name]['#options'] = $contact_field['options'];
+          $form['contact_fields'][$contact_field_name]['#options'] = $contact_field['options'];
           if (empty($contact_field['required'])) {
-            $form['contact-fields'][$contact_field_name]['#empty_option'] = t('- None -');
+            $form['contact_fields'][$contact_field_name]['#empty_option'] = t('- None -');
           }
         }
       }
--- src/Form/RequestLinkForm.php
+++ src/Form/RequestLinkForm.php
@@ -122,7 +122,7 @@ class RequestLinkForm extends FormBase {
       // Add contact fields.
       foreach ($profile->contact_fields as $contact_field_name => $contact_field) {
         if (isset($contact_field['active']) && $contact_field['active']) {
-          $form['contact-fields'][$contact_field_name] = array(
+          $form['contact_fields'][$contact_field_name] = array(
             '#type' => Utils::getContactFieldType($contact_field),
             '#title' => $contact_field['label'],
             '#description' => $contact_field['description'],
@@ -132,9 +132,9 @@ class RequestLinkForm extends FormBase {
                       ? $contact_field['weight'] : 0,
           );
           if (!empty($contact_field['options'])) {
-            $form['contact-fields'][$contact_field_name]['#options'] = $contact_field['options'];
+            $form['contact_fields'][$contact_field_name]['#options'] = $contact_field['options'];
             if (empty($contact_field['required'])) {
-              $form['contact-fields'][$contact_field_name]['#empty_option'] = t('- None -');
+              $form['contact_fields'][$contact_field_name]['#empty_option'] = t('- None -');
             }
           }
         }
